cabeed41d4d013b58964e7ae4120a0371e7bcfdf
SirBarto <bartho58@gmail.com>
2020-11-16 19:40:42 +0100

    LineChart and added field for DataAG

--- app/src/main/java/polsl/p/helloworld/DataAG.java
+++ app/src/main/java/polsl/p/helloworld/DataAG.java
@@ -4,20 +4,30 @@ import androidx.annotation.NonNull;
 
 public class DataAG {
 
-    final private String xAccelerometer,yAccelerometer,zAccelerometer,xGyroscope,yGyroscope,zGyroscope;
+    final private String xAccelerometer,yAccelerometer,zAccelerometer,
+            xGyroscope,yGyroscope,zGyroscope;
+    private int scope;
+    private float timeStamp;
+    private double omegaMagnitudeAccelerometer,omegaMagnitudeGyroscope,speedV;
 
-    public DataAG(String xAccelerometer, String yAccelerometer, String zAccelerometer, String xGyroscope, String yGyroscope, String zGyroscope) {
+    public DataAG(String xAccelerometer, String yAccelerometer, String zAccelerometer, String xGyroscope, String yGyroscope, String zGyroscope,
+                  int scope, float timeStamp, double omegaMagnitudeAccelerometer, double omegaMagnitudeGyroscope, double speedV) {
         this.xAccelerometer = xAccelerometer;
         this.yAccelerometer = yAccelerometer;
         this.zAccelerometer = zAccelerometer;
         this.xGyroscope = xGyroscope;
         this.yGyroscope = yGyroscope;
         this.zGyroscope = zGyroscope;
+        this.scope = scope;
+        this.timeStamp = timeStamp;
+        this.omegaMagnitudeAccelerometer = omegaMagnitudeAccelerometer;
+        this.omegaMagnitudeGyroscope = omegaMagnitudeGyroscope;
+        this.speedV = speedV;
     }
 
     @NonNull
     @Override
     public String toString() {
-        return "\n"+xAccelerometer+","+yAccelerometer+","+zAccelerometer+","+xGyroscope+","+yGyroscope+","+zGyroscope;
+        return "\n"+xAccelerometer+","+yAccelerometer+","+zAccelerometer+","+xGyroscope+","+yGyroscope+","+zGyroscope+","+scope+","+timeStamp+","+ omegaMagnitudeAccelerometer +","+omegaMagnitudeGyroscope+","+speedV;
     }
 }
